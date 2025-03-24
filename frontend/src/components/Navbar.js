ffe1ed75e74cd00c71fb8ed57620457ab199fa78
Darshan112224 <137253877+Darshan112224@users.noreply.github.com>
2025-03-24 11:16:41 +0530

    Updated dependencies

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,10 +7,9 @@ const Navbar = () => (
         <div className="nav-container">
             <Link to="/" className="nav-logo">Darshan Patel</Link>
             <div className="nav-links">
-                <Link to="/">Home</Link>
-                
+                <li><a href="mailto:darshan.1116.patel@gmail.com.com">Contact Me</a></li>
                 <a href="/resume_darshan_patel.pdf" target="_blank" class="resume-link">Resume</a>
-
+                
             </div>
         </div>
     </nav>
